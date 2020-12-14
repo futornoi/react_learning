90b40206a040b68f7a0b2212dd1f65d69a70ee46
Maks <makism.futornoi@mail.ru>
2020-12-14 20:30:43 +0200

    lesson 52 'function || class Users.jsx'

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -5,44 +5,50 @@ import * as axios from 'axios';
 import avatar from '../../assets/images/icon.png'
 
 
-function Users(props) {
-   if (props.users.length === 0) {
+class Users extends React.Component{
+   constructor(props) {
+      super(props);
+
+
       axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
          props.setUsers(response.data.items)
       })
    }
 
-
-   return <div>
-      <h2 className={s.title}>Users</h2>
-      {props.users.map((u) => <div className={style.following} key={u.id}>
-         <div className={style.mainInfo}>
-            <div>
-               <div className={style.avatar}>
-                  <img src={u.photos.large != null ? u.photos.large : avatar} alt=""/>
-               </div>
-            </div>
-
-            <div className={style.aboutUser}>
-               <div>
-                  <div>{u.name}</div>
-                  <div className={style.userStatus}>{u.status}</div>
-               </div>
-               <div className={style.location}>
-                  <div>{'u.location.country'},</div>
-                  <div>{'u.location.city'}</div>
-               </div>
-            </div>
-         </div>
+   render() {
+      return (
          <div>
-            {u.following
-               ? <button className={style.btn} onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-               : <button className={style.btn} onClick={() => {props.followed(u.id)}}>Follow</button>
-            }
-         </div>
-         </div>)}
-      </div>
+            <h2 className={s.title}>Users</h2>
+            {this.props.users.map((u) => <div className={style.following} key={u.id}>
+               <div className={style.mainInfo}>
+                  <div>
+                     <div className={style.avatar}>
+                        <img src={u.photos.large != null ? u.photos.large : avatar} alt=""/>
+                     </div>
+                  </div>
 
-      }
+                  <div className={style.aboutUser}>
+                     <div>
+                        <div>{u.name}</div>
+                        <div className={style.userStatus}>{u.status}</div>
+                     </div>
+                     <div className={style.location}>
+                        <div>{'u.location.country'},</div>
+                        <div>{'u.location.city'}</div>
+                     </div>
+                  </div>
+               </div>
+               <div>
+                  {u.following
+                     ? <button className={style.btn} onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>
+                     : <button className={style.btn} onClick={() => {this.props.followed(u.id)}}>Follow</button>
+                  }
+               </div>
+            </div>)}
+         </div>
+
+      )
+   }
+}
 
       export default Users;
--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -1,6 +1,7 @@
-import Users from "./Users";
 import {connect} from "react-redux";
 import {followAC, setUsersAC, unfollowAC} from "../../redux/users-reducer";
+import Users from "./Users";
+
 
 
 
