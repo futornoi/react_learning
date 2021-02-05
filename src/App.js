150eb9c8c60bb7f066beaf0d145fac5905d059ed
Maks <makism.futornoi@mail.ru>
2021-02-05 13:51:19 +0200

    lesson 94 'React.lazy || React.Suspense'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import Navigation from './Components/Nav/Nav';
 import News from "./Components/News/News";
 import MessagesContainer from "./Components/Messages/MessagesContainer";
-import UsersContainer from "./Components/Users/UsersContainer";
-import ProfileContainer from "./Components/Profile/ProfileContainer";
+// import UsersContainer from "./Components/Users/UsersContainer";
+// import ProfileContainer from "./Components/Profile/ProfileContainer";
 import HeaderContainer from "./Components/Header/HeaderContainer";
 import Login from "./Components/Login/Login";
 import {connect} from "react-redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./Components/common/Preloader/Preloader";
+import {withSuspense} from "./hoc/withSuspense";
 
-
+const ProfileContainer = React.lazy(() => import("./Components/Profile/ProfileContainer"));
+const UsersContainer = React.lazy(() => import("./Components/Users/UsersContainer"));
 
 class App extends React.Component {
 
@@ -22,7 +24,7 @@ class App extends React.Component {
 
    render() {
 
-      if(!this.props.initialization) return <Preloader/>
+      if (!this.props.initialization) return <Preloader/>
 
       return (
          <div className="app-wrapper">
@@ -30,15 +32,13 @@ class App extends React.Component {
             <Navigation/>
             <div className="app-wrapper-content">
                {/*Profile */}
-               <Route path='/profile/:userId?' render={() => <ProfileContainer/>}
-               />
+               <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
                {/*Messages*/}
                <Route path='/messages' render={() => <MessagesContainer/>}/>
                {/* News */}
                <Route path='/news' render={() => <News/>}/>
                {/*Users*/}
-               <Route path='/users' render={() => <UsersContainer/>}/>
-
+               <Route path='/users' render={withSuspense(UsersContainer)}/>
                <Route path='/login' render={() => <Login/>}/>
             </div>
          </div>
